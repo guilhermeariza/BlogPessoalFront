616fcab4ef545861e051f054db82e45b03672c8a
Guilherme Ariza <gui.ariza@gmail.com>
2022-06-15 20:48:46 -0300

    component tema sem consumo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { TemaComponent } from './tema/tema.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { EntrarComponent } from './entrar/entrar.component';
@@ -8,7 +9,8 @@ const routes: Routes = [
   {path:'', redirectTo:'entrar', pathMatch:'full'},
   {path: 'entrar', component:EntrarComponent},
   {path: 'cadastrar', component:CadastrarComponent},
-  {path: 'inicio', component:InicioComponent}
+  {path: 'inicio', component:InicioComponent},
+  {path: 'tema', component:TemaComponent},
 
 ];
 
